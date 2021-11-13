53ba120fe1f9fc42214ae6e5650b3ca7fc4c0a8c
sntsabode <snts.abode@gmail.com>
2021-11-13 02:16:19 +0200

    feat(app): get app name from process arguments

--- src/index.js
+++ src/index.js
@@ -6,13 +6,28 @@ const { askAndWait } = require('./core/utils')
 async function main() {
   const cwd = process.cwd()
 
-  console.log('Please enter you app\'s name...')
-  const appName = await askAndWait()
+  const appName = await getAppName()
 
   const core = new Core(cwd, appName)
   const exitCode = await core.main()
 
   process.exit(exitCode)
+}
+
+async function getAppName() {
+  const args = process.argv.slice(2)
+
+  if (args.length > 1) {
+    console.error('Too many arguments.')
+    console.error('Please provide only one argument; your app\'s name')
+    process.exit(1)
+  } else if (args.length === 1) {
+    return args[0]
+  }
+
+  console.log('Please enter you app\'s name...')
+
+  return askAndWait()
 }
 
 (async () => main())()
